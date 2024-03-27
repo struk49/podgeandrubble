e3fc51b2e2a919032b8ea2949a6c177df0a53070
struk49 <andrewstruk@yahoo.co.uk>
2024-03-27 17:20:08 +0000

    completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
@@ -34,7 +33,7 @@ def checkout(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        basket = request.session.get('basket', {})
+        basket= request.session.get('basket', {})
 
         form_data = {
             'full_name': request.POST['full_name'],
@@ -49,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
             for item_id, item_data in basket.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -66,7 +69,7 @@ def checkout(request):
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                product_size=size,
+                                product_color=color,
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
