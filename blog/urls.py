495fb2748446daa777a1916ccb821d1fd92347ab
struk49 <andrewstruk@yahoo.co.uk>
2024-03-31 11:01:50 +0000

    added blog detsil, urls, views and template

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_posts, name='posts')
+    path('', views.all_posts, name='posts'),
+    path('<post_id>', views.blog_detail, name='blog_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog_post
 
 # Create your views here.
 
 def all_posts(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all posts, including sorting and search queries """
 
     posts = Blog_post.objects.all()
 
@@ -13,3 +13,15 @@ def all_posts(request):
     }
 
     return render(request, 'blog/blog_post.html', context)
+
+
+def blog_detail(request, post_id):
+    """ A view to show individual post details """
+
+    post = get_object_or_404(Blog_post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
