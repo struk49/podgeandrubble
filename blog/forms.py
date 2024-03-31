4db0f797a4f9235264f5854d8279034062e9cb0d
struk49 <andrewstruk@yahoo.co.uk>
2024-03-31 13:15:49 +0000

    added blogform

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Blog_post, Category
+
+
+class BlogForm(forms.ModelForm):
+
+    class Meta:
+        model = Blog_post
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
