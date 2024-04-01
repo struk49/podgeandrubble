c3cf432997a8d1c78411cdf5b0f2ec25c5715276
struk49 <andrewstruk@yahoo.co.uk>
2024-04-01 22:47:05 +0000

    deploying to heroku

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n&9i4rdxmq6jt#pv$3ut)&1n^$_$ev)32572_5=j4z+6a585ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['podgeandrubbleveg-3192dfebb79e.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['podgeandrubbleveg.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
