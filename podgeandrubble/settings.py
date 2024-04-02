dc12becf6699b6d0f477fea892cb05a97a7f4006
struk49 <andrewstruk@yahoo.co.uk>
2024-04-02 12:27:24 +0000

    deploying

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'w-+r8audnz%d%ixuq#u+0vm*2#__lb7c+np@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['podgeandrubbleveg-3192dfebb79e.herokuapp.com', 'localhost', '8000-struk49-podgeandrubble-xjeeyoghr9i.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['podgeandrubbleveg-1f49cce16286.herokuapp.com', 'localhost', '8000-struk49-podgeandrubble-xjeeyoghr9i.ws-eu110.gitpod.io']
 
 
 # Application definition
