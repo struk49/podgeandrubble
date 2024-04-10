4f256bf100beb8a8a6077da1d8146b5660c20d37
struk49 <andrewstruk@yahoo.co.uk>
2024-04-10 14:28:05 +0000

    trying to add email

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -221,7 +221,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'podgeandrubble@example.com'
+    DEFAULT_FROM_EMAIL = 'EMAIL_HOST_USER'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
